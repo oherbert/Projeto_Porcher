01bc6866ecf02aa4402d8093929ab9d4dc2438f0
oherbert <o_herbert@hotmail.com>
2019-02-04 01:07:25 -0200

    ViewConfiguração criada, sem testar

--- src/Util/Config.java
+++ src/Util/Config.java
@@ -6,14 +6,73 @@
 package Util;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+import java.util.TreeMap;
 
 /**
  *
  * @author Herbert
  */
 public class Config {
+    
+    private String parametro;
+    private String valor;
+
+    public Config(String parametro, String valor) {
+        this.parametro = parametro;
+        this.valor = valor;
+    }
+
+    public String getParametro() {
+        return parametro;
+    }
+
+    public void setParametro(String parametro) {
+        this.parametro = parametro;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 23 * hash + Objects.hashCode(this.parametro);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Config other = (Config) obj;
+        if (!Objects.equals(this.parametro, other.parametro)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+    
 
     public static String loadConfig(Enum ConfigList) {
 
@@ -37,8 +96,34 @@ public class Config {
         return null;
     }
     
-    public static void setConfig (String parameter, String newValue){
+    public static void setConfig (Config config){
+        String path = "Config.txt";
+        Map<String, String> mapConfig = new TreeMap<>();
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
             
+            while (line != null) {
+                String[] linha = line.split(": ");
+                mapConfig.put(linha[0], linha[1]);
+                line = br.readLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+         try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            mapConfig.put(config.getParametro(), config.getValor());
+            
+            for (String key: mapConfig.keySet() ){
+            bw.write(key + " ,"+ mapConfig.get(key));
+            bw.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao tentar alterar as configuraçoes : "+ e.getMessage() );
+        }
+        
+    }
     }
 
-}
--- src/view/ViewTemperatura.java
+++ src/view/ViewTemperatura.java
@@ -118,6 +118,8 @@ public class ViewTemperatura extends javax.swing.JFrame {
         lblGrafico = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
 
@@ -242,9 +244,31 @@ public class ViewTemperatura extends javax.swing.JFrame {
         );
 
         jMenu1.setText("Menu");
+
+        jMenuItem2.setText("Graficos");
+        jMenuItem2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jMenuItem2MouseClicked(evt);
+            }
+        });
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem2);
+
+        jMenuItem3.setText("Configurações");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem3);
+
         jMenuBar1.add(jMenu1);
 
-        jMenu2.setText("Help");
+        jMenu2.setText("Ajuda");
 
         jMenuItem1.setText("Registros");
         jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
@@ -292,6 +316,19 @@ public class ViewTemperatura extends javax.swing.JFrame {
         ViewHelp viewHelp = new ViewHelp(this, rootPaneCheckingEnabled);
         viewHelp.setVisible(rootPaneCheckingEnabled);
     }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void jMenuItem2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenuItem2MouseClicked
+        
+    }//GEN-LAST:event_jMenuItem2MouseClicked
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        ViewConfiguracao viewConfiguracao = new ViewConfiguracao(this, rootPaneCheckingEnabled);
+        viewConfiguracao.setVisible(rootPaneCheckingEnabled);
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -368,6 +405,8 @@ public class ViewTemperatura extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JLabel lblEstado;
